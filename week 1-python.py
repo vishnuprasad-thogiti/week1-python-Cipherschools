3d3ed8f6753b0307d7856381211b0de5380b3882
vishnuprasad-thogiti <115465760+vishnuprasad-thogiti@users.noreply.github.com>
2022-11-30 15:58:31 +0530

    Add files via upload

---
+++ week 1-python.py
@@ -0,0 +1,531 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[4]:
+
+
+a = "a"
+
+
+# In[5]:
+
+
+type(a)
+
+
+# In[6]:
+
+
+isinstance('a',str)
+
+
+# In[7]:
+
+
+isinstance('a',int)
+
+
+# In[9]:
+
+
+isinstance(print,object)
+
+
+# In[10]:
+
+
+a = 5
+
+
+# In[11]:
+
+
+print(id(a))
+
+
+# In[12]:
+
+
+print(hex(id(a)))
+
+
+# In[13]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+a = 'aaaaa'
+
+
+# In[14]:
+
+
+print(id(a))
+
+
+# In[16]:
+
+
+b = 'aaaaa'
+
+
+# In[17]:
+
+
+print(id(b))
+
+
+# In[18]:
+
+
+a = 2345676545678890
+
+
+# In[19]:
+
+
+print(a+1)
+
+
+# In[22]:
+
+
+print(1,2,3, "tony",3.4,1 + 3j)
+
+
+# In[23]:
+
+
+print("hello")
+print("world :")
+
+
+# In[24]:
+
+
+print("hello" , end=";")
+print("world :")
+
+
+# In[25]:
+
+
+print(1,2,3, "tony" , 3.4, 1+3j , sep=",")
+
+
+# In[28]:
+
+
+a = input()
+
+
+# In[30]:
+
+
+print(a)
+
+
+# In[31]:
+
+
+type(a)
+
+
+# In[32]:
+
+
+a = 15
+
+
+# In[33]:
+
+
+int('123')
+
+
+# In[34]:
+
+
+float('1.9')
+
+
+# In[35]:
+
+
+1+2
+
+
+# In[36]:
+
+
+2-1
+
+
+# In[38]:
+
+
+10/3
+
+
+# In[39]:
+
+
+10//3
+
+
+# In[40]:
+
+
+2*3
+
+
+# In[41]:
+
+
+2**3
+
+
+# In[42]:
+
+
+10%4
+
+
+# In[44]:
+
+
+"vishnu" + "prasad"
+
+
+# In[45]:
+
+
+"tony" " " "stark"
+
+
+# In[46]:
+
+
+"vishnu" * 3
+
+
+# In[47]:
+
+
+1 == 1
+
+
+# In[48]:
+
+
+1 != 2
+
+
+# In[49]:
+
+
+1 <2
+
+
+# In[50]:
+
+
+1 > 2
+
+
+# In[51]:
+
+
+1 <= 2
+
+
+# In[52]:
+
+
+1 >= 2
+
+
+# In[53]:
+
+
+"a" < "x"
+
+
+# In[54]:
+
+
+"a" < "A"
+
+
+# In[55]:
+
+
+True and False
+
+
+# In[56]:
+
+
+True or False
+
+
+# In[57]:
+
+
+True and 1
+
+
+# In[58]:
+
+
+1  or 0
+
+
+# In[59]:
+
+
+1 and 5
+
+
+# In[60]:
+
+
+"" and 4
+
+
+# In[61]:
+
+
+1.9 and ""
+
+
+# In[62]:
+
+
+"" or 6
+
+
+# In[63]:
+
+
+5 or ""
+
+
+# In[18]:
+
+
+a = 5
+
+if a == 3:
+    print("this value is 3")
+elif a == 5:
+    print("this value is 5")
+else:
+    print("this value is not 3 or 5")
+
+
+# In[64]:
+
+
+a = 1
+while a < 10:
+  print(a)
+  a += 1
+
+
+# In[66]:
+
+
+name = "vishnu"
+
+
+# In[67]:
+
+
+name._iter_
+
+
+# In[68]:
+
+
+print(type(name))
+for c in name:
+    print(c)
+    print(type(c))
+
+
+# In[69]:
+
+
+for i in range(9):
+  print(i)
+
+
+# In[70]:
+
+
+a = range(9)
+
+
+# In[71]:
+
+
+a
+
+
+# In[72]:
+
+
+a._iter_
+
+
+# In[73]:
+
+
+"aaaa" * 4
+
+
+# In[74]:
+
+
+for i in range(5):
+  print(i)
+  if i == 3:
+       break
+
+
+# In[75]:
+
+
+for i in range(5):
+  print(i)
+  i = 100
+  print(i)
+
+
+# In[76]:
+
+
+if True:
+  pass
+print("rest of the code")
+
+
+# In[77]:
+
+
+n = 5
+
+
+# max(1,2,3,4,5)
+
+# In[78]:
+
+
+n = 5
+for i in range(n):
+  for j in range(n):
+     print(max(i+1, j+1, n-i, n-j), end = " ")
+  print()
+
+
+# In[79]:
+
+
+n = 5
+for i in range(n):
+  for j in range(n):
+     print(min(i, j), end = " ")
+  print()
+
+
+# In[80]:
+
+
+n = 5
+for i in range(n):
+  for j in range(n):
+     print((n-i-1), end = " ")
+  print()
+
+
+# In[81]:
+
+
+n = 5
+for i in range(n):
+  for j in range(n):
+     print((n-j-1), end = " ")
+  print()
+
+
+# In[82]:
+
+
+n = 5
+for i in range(n):
+  for j in range(n):
+     print(
+           max(max(i+1 ,j+1), max(n-j, n-i)
+     ), end = " ")
+  print()
+
+
+# In[83]:
+
+
+sum([1, 2, 3, 4, 5, 6, 7, 8, 0])
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[19]:
+
+
+x: int(-inf, inf)
+g -> x
+
+a = x <0
+b = x ==0
+c = x> 0
+
+a int b = b int c = a int c = phi
+
+a u b u c ! G
+
+conditions are mutually exclusive
+
+
+# In[14]:
+
+
+q  =  can profile a acces profile a
+
+a = 
+
+
+# In[ ]:
+
+
+
+
